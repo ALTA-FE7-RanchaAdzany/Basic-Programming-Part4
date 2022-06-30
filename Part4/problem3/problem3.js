28675d9feac48a7b6979b0cae1b7b2f58301177d
ikram abdillah <ikram.koli@gmail.com>
2022-06-30 21:29:21 +0700

    Testing

--- Part4/problem3/problem3.js
+++ Part4/problem3/problem3.js
@@ -1,5 +1,11 @@
 function joinArrayRemoveDuplicate(arrayA, arrayB) {
   // Your Code Here
+  let result = [...arrayA, ...arrayB];
+  return result;
 }
 
-module.exports = joinArrayRemoveDuplicate;
+console.log(joinArrayRemoveDuplicate(["apel", "anggur"], ["lemon", "leci", "nanas"]));
+console.log(joinArrayRemoveDuplicate(["samsung", "apple"], ["apple", "sony", "xiaomi"]));
+console.log(joinArrayRemoveDuplicate(["football", "basketball"], ["basketball", "football"]));
+
+// module.exports = joinArrayRemoveDuplicate;
